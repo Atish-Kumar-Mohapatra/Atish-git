9bf4025bf0df491394b02c7ac4079c92d1a17c6c
Atish Mohapatra <atishpinku72@gmail.com>
2019-02-18 21:47:34 +0530

    JavaScript

---
+++ js/code.js
@@ -0,0 +1,11 @@
+console.log(Number.MAX_VALUE + " & " + Number.MIN_VALUE);
+var a = 10;
+console.log(a || true);
+console.log('-1' == false);
+console.log(typeof a);
+
+//Java Script objects
+
+
+//Scope
+console.log(history);
